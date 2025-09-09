adcf188a5adda86a962edef880f7e461937ff25e
guru <anedost@gmail.com>
2025-09-09 07:45:03 +0300

    add hw 2

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,57 @@
+import * as contactsService from '../services/contactsServices.js';
+
+import HttpError from '../helpers/HttpError.js';
+
+import controllerWrapper from '../helpers/controllerWrapper.js';
+
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.getContacts();
+
+  res.json(result);
+};
+
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404, `Contact with id:${id} not found`);
+  }
+
+  res.json(result);
+};
+
+const createContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+
+  res.status(201).json(result);
+};
+
+const updateContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.updateContactById(id, req.body);
+
+  if (!result) {
+    throw HttpError(404, `Contact with id = ${id} not found `);
+  }
+
+  res.json(result);
+};
+
+const deleteContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.deleteContactById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id = ${id} not found `);
+  }
+
+  res.json(result);
+};
+
+export default {
+  getAllContacts: controllerWrapper(getAllContacts),
+  getOneContact: controllerWrapper(getOneContact),
+  deleteContactById: controllerWrapper(deleteContactById),
+  createContact: controllerWrapper(createContact),
+  updateContactById: controllerWrapper(updateContactById),
+};
---
+++ routes/contactsRouter.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import contactsControllers from '../controllers/contactsControllers.js';
+
+import validateBody from '../helpers/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../schemas/contactsSchemas.js';
+
+const contactsRouter = express.Router();
+
+contactsRouter.get('/', contactsControllers.getAllContacts);
+
+contactsRouter.get('/:id', contactsControllers.getOneContact);
+
+contactsRouter.delete('/:id', contactsControllers.deleteContactById);
+
+contactsRouter.post(
+  '/',
+  validateBody(createContactSchema),
+  contactsControllers.createContact
+);
+
+contactsRouter.put(
+  '/:id',
+  validateBody(updateContactSchema),
+  contactsControllers.updateContactById
+);
+
+export default contactsRouter;
